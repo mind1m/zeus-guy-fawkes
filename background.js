a1805ab6552537c1ffca8449ae2093e53a66feff
Ivan <aiver@ua.fm>
2012-08-06 11:02:12 +0300

    image, background class

---
+++ background.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: V
+ * Date: 06.08.12
+ * Time: 10:54
+ * To change this template use File | Settings | File Templates.
+ */
+function backGround() {
+
+}
