e1a8ea2e4f82c1a935a3722595c1b89d31c7568a
Ivan <aiver@ua.fm>
2012-08-06 12:23:50 +0300

    image, background class

--- background.js
+++ background.js
@@ -5,6 +5,49 @@
  * Time: 10:54
  * To change this template use File | Settings | File Templates.
  */
-function backGround() {
+function BackGround() {
+    var tiles = new Array();
+    var sizeOfTile;
+    var sizeOfMap;
+    var type = 1;
+    var numberOfTiles;
 
+    this.getTiles = function() {
+        return tiles;
+    };
+    this.getSizeOfTile = function() {
+        return sizeOfTile;
+    };
+    this.getSizeOfMap = function() {
+        return sizeOfMap;
+    };
+    this.getType = function() {
+        return type;
+    };
+    this.getNumberOfTiles = function() {
+        return numberOfTiles;
+    };
+    this.setTiles = function(newTiles) {
+        tiles = newTiles;
+    };
+    this.setSizeOfTile = function(newSizeOfTile) {
+        sizeOfTile = newSizeOfTile;
+    };
+    this.setSizeOfMap = function(newSizeOfMap) {
+        sizeOfMap = newSizeOfMap;
+    };
+    this.setNumberOfTiles = function(newNumberOfTiles) {
+        numberOfTiles = newNumberOfTiles;
+    };
+
+    this.constructor = function(newSizeOfMap, newSizeOfTile) {
+        numberOfTiles = newSizeOfMap/newSizeOfTile;
+        sizeOfMap = newSizeOfMap;
+        sizeOfTile = newSizeOfTile;
+        for (var i = 0; i < numberOfTiles; ++i) {
+            for (var j = 0; j < numberOfTiles; ++j) {
+                tiles[i, j] = type;
+            }
+        }
+    };
 }
--- draw.js
+++ draw.js
@@ -5,3 +5,18 @@
  * Time: 11:18
  * To change this template use File | Settings | File Templates.
  */
+function draw(backGround) {
+    var element;
+    for (var i = 0; i < backGround.getNumberOfTiles(); ++i) {
+        for (var j = 0; j < backGround.getNumberOfTiles(); ++j) {
+            element = document.createElement('div');
+            element.style.position = "absolute";
+            element.style.left = backGround.getSizeOfTile() * i + "px";
+            element.style.top = backGround.getSizeOfTile() * j + "px";
+            element.style.width = backGround.getSizeOfTile() + "px";
+            element.style.height = backGround.getSizeOfTile() + "px";
+            element.style.backgroundImage = "url(images/background.gif)";
+            document.getElementsByTagName("body")[0].appendChild(element);
+        }
+    }
+}
