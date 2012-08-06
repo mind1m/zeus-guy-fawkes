e2e6eadb62b10682c556997980f63ef57a177af0
Ivan <aiver@ua.fm>
2012-08-06 19:23:43 +0300

    Implemented z-index

--- createObjects.js
+++ createObjects.js
@@ -9,17 +9,20 @@ const sizeOfMap = 256;
 const sizeOfTile = 32;
 const numberOfTiles = sizeOfMap/sizeOfTile;
 const blockImage = "url(images/block.gif)";
-const backGroundImage = "url(images/background.gif)";
+const backgroundImage = "url(images/background.gif)";
+const backgroundIndex = 1;
+const blocksIndex = 2;
 
 function createObjects(backGround, blocks) {
     var element;
     //Background
-    var backGroundDiv = document.createElement('div');
-    backGroundDiv.setAttribute('id', 'background');
-    backGroundDiv.style.position = "absolute";
-    backGroundDiv.style.left = "0px";
-    backGroundDiv.style.top = "0px";
-    document.getElementById("field").appendChild(backGroundDiv);
+    var backgroundDiv = document.createElement('div');
+    backgroundDiv.setAttribute('id', 'background');
+    backgroundDiv.style.position = "absolute";
+    backgroundDiv.style.left = "0px";
+    backgroundDiv.style.top = "0px";
+    backgroundDiv.style.zIndex = backgroundIndex;
+    document.getElementById("field").appendChild(backgroundDiv);
     for (var i = 0; i < backGround.length; ++i) {
         element = document.createElement('div');
         element.setAttribute('id', 'partOfBackground');
@@ -28,8 +31,9 @@ function createObjects(backGround, blocks) {
         element.style.top = sizeOfTile * backGround[i].getY() + "px";
         element.style.width = sizeOfTile + "px";
         element.style.height = sizeOfTile + "px";
-        element.style.backgroundImage = backGroundImage;
-        backGroundDiv.appendChild(element);
+        element.style.backgroundImage = backgroundImage;
+        //element.style.zIndex = backgroundIndex;
+        backgroundDiv.appendChild(element);
     }
     //Blocks
     var blocksDiv = document.createElement('div');
@@ -37,6 +41,7 @@ function createObjects(backGround, blocks) {
     blocksDiv.style.position = "absolute";
     blocksDiv.style.left = "0px";
     blocksDiv.style.top = "0px";
+    blocksDiv.style.zIndex = blocksIndex;
     document.getElementById("field").appendChild(blocksDiv);
     for (var i = 0; i < blocks.length; ++i) {
         element = document.createElement('div');
@@ -47,6 +52,7 @@ function createObjects(backGround, blocks) {
         element.style.width = sizeOfTile + "px";
         element.style.height = sizeOfTile + "px";
         element.style.backgroundImage = blockImage;
+        //element.style.zIndex = blocksIndex;
         blocksDiv.appendChild(element);
     }
 }
