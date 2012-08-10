7352bf4a8f3b095538d0bafae63b1f29c695d214
Evgeniy <zheka238@gmail.com>
2012-08-10 11:44:11 +0300

    zamena

--- createObjects.js
+++ createObjects.js
@@ -5,7 +5,7 @@
  * Time: 11:18
  * To change this template use File | Settings | File Templates.
  */
-const sizeOfMap = 400;
+const sizeOfMap = 384;
 const sizeOfTile = 32;
 const numberOfTiles = sizeOfMap/sizeOfTile;
 const blockImage = "url(images/block.gif)";
