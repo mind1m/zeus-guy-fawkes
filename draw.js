08a46af1be5bf85024bb67d6a4cecfc75090f6ca
Anton Kasyanov <mind1master@gmail.com>
2012-08-16 11:57:13 +0300

    fixing stuff

--- draw.js
+++ draw.js
@@ -183,8 +183,8 @@ function resize(){
         //sizeOfMap = Math.round(sizeOfMap * 0.8);
         //sizeOfTile = Math.round(sizeOfTile * 0.8);
     } else if (body_width < body_height && sizeOfMap + controls.offsetHeight > body_height) {
-        //sizeOfMap = Math.round(sizeOfMap * 0.7);
-       // sizeOfTile = Math.round(sizeOfTile * 0.7);
+        sizeOfMap = Math.round(sizeOfMap * 0.7);
+        sizeOfTile = Math.round(sizeOfTile * 0.7);
     }
 
 
